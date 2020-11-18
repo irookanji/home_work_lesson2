e2af7678e1852a0026688bac92ef0a5d14a34063
Irookanji <murderer187@mail.ru>
2020-11-18 23:30:51 +0100

    Create AlfabankTests for lesson3, wrote first test

--- src/test/java/tests/lesson3/AlfabankTests.java
+++ src/test/java/tests/lesson3/AlfabankTests.java
@@ -4,7 +4,9 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class AlfabankTests {
     @BeforeAll
@@ -15,6 +17,30 @@ public class AlfabankTests {
     @Test
     void checkDepositTest() {
         open("https://alfabank.ru/make-money/");
+
+        // Go to Deposits tab
+        $$(byText("Депозиты")).find(visible).parent().click();
+
+        // Go to Archived debts & deposits tab
+        $$(byText("Архивные счета и депозиты")).find(visible).parent().click();
+
+        // Choose Deposits tab
+        $$(byText("Депозиты")).find(visible).parent().click();
+
+        // Check that Deposits = 5
+        $("#filter").$$("[data-widget-name=CatalogCard]").shouldHaveSize(5);
+
+
+        sleep(5000);
+
+    }
+
+    @Test
+    void goToDepositsThenToInsuranceTest() {
+        open("https://alfabank.ru");
+
+        // Go to "Вклады" tab
+        $$(byText("Вклады")).find(visible).click();
 
     }
 
